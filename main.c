2c40d2021bf8cb43aaaa50584d155e758641b7ae
dosleam <9400@holbertonstudents.com>
2024-08-21 13:21:32 +0200

    add main.c for which launches the loop functions

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * shell_loop - Boucle principale du shell
+ * Description: Gère l'entrée de l'utilisateur et exécute les commandes
+ * Return: Rien
+ */
+
+void shell_loop(void);
+
+/**
+ * main - Entrée principale du programme
+ * Return: Toujours 0
+ */
+int main(void)
+{
+	shell_loop();
+	return (0);
+}
