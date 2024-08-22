a17fecf6d378930f04976191d7ca2ee7b923efda
dosleam <9400@holbertonstudents.com>
2024-08-22 10:28:51 +0200

    modification of the code to remove the prohibited functions and replace them

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 extern char **environ;
 
@@ -17,8 +18,9 @@ char **parse_command(char *line);
 void handle_command(char **args);
 char *get_command_path(char *command_name);
 void shell_loop(void);
-void run_child(int fd_in, int fd[2], char **args, int next_command);
+void run_child(int fd_in, int fd_out, char **args);
 char *build_path(char *path, char *cmd);
 void execute_pipeline(char **commands);
+void generate_temp_filename(int index, char *buffer);
 
 #endif /* MAIN_H */
