65c7b8c4ee7ec67e38f016267ed36d7bf9d8205b
dosleam <9400@holbertonstudents.com>
2024-08-22 10:31:18 +0200

    modified code to respect betty

--- execute_pipeline.c
+++ execute_pipeline.c
@@ -4,7 +4,8 @@
  * generate_temp_filename - Génère un nom de fichier temporaire unique
  * @index: Indice pour différencier les fichiers temporaires
  * @buffer: Buffer pour stocker le nom de fichier généré
- * Description: Utilise l'indice pour générer un nom de fichier temporaire unique.
+ * Description: Utilise l'indice pour générer un nom
+ * de fichier temporaire unique.
  * Return: Rien
  */
 void generate_temp_filename(int index, char *buffer)
@@ -29,13 +30,13 @@ void run_child(int fd_in, int fd_out, char **args)
 		close(STDIN_FILENO);
 		open("/dev/null", O_RDONLY);
 	}
-	
+
 	if (fd_out != STDOUT_FILENO)
 	{
 		close(STDOUT_FILENO);
 		open("/dev/null", O_WRONLY);
 	}
-	
+
 	execve(args[0], args, environ);
 	perror("execve");
 	exit(EXIT_FAILURE);
@@ -56,20 +57,17 @@ void execute_pipeline(char **commands)
 	char **args;
 	char temp_file[20];
 	int file_index = 0;
-	
+
 	while (*commands)
 	{
 		generate_temp_filename(file_index++, temp_file);
 		fd_out = open(temp_file, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-		
 		if (fd_out == -1)
 		{
 			perror("open");
 			exit(EXIT_FAILURE);
 		}
-		
 		args = parse_command(*commands);
-		
 		pid = fork();
 		if (pid == -1)
 		{
